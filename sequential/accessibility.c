6e1e9c3b88b194377fb2997f40cde1a79f5bca22
Jeff Xu <jiaxijeffxu@gmail.com>
2021-04-10 22:15:33 -0400

    starter sequential files

---
+++ sequential/accessibility.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <assert.h>
+#include <stdint.h>
+#include <error.h>
+#include <limits.h>
+
+// step 1: figure out how to integrate Libxml2
+// step 2: figure out how to read input

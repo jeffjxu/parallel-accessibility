ce28042f39546dba9468dde143b01b9f6e143d96
Jeff Xu <jiaxijeffxu@gmail.com>
2021-05-02 21:01:31 -0400

    working naive openmp implementation

--- sequential/accessibility.c
+++ sequential/accessibility.c
@@ -40,17 +40,17 @@ void check_alt_text(xmlNode *node) {
     // {
     //     #pragma omp single 
     //     {
-            xmlAttr *property = node->properties;
-            while (property != NULL) {
-                const xmlChar *name = property->name;
-                // #pragma omp task
-                if (strcmp((const char*)name, "alt") == 0) {
-                    xmlChar *value = xmlGetProp(node, name);
-                    if (strcmp((const char*)value, "") != 0) {
-                        ALT_COUNT++;
-                    }
-                }
-                property = property->next;
+    xmlAttr *property = node->properties;
+    while (property != NULL) {
+        const xmlChar *name = property->name;
+        // #pragma omp task
+        if (strcmp((const char*)name, "alt") == 0) {
+            xmlChar *value = xmlGetProp(node, name);
+            if (strcmp((const char*)value, "") != 0) {
+                ALT_COUNT++;
+            }
+        }
+        property = property->next;
         //     }
         // }
         //#pragma omp taskwait // may not be needed for this funciton
@@ -71,24 +71,62 @@ bool check_if_alt_needed(xmlNode *node) {
 // :input node (xmlNode*) - a parsed node of the DOM tree
 // :input depth (int) - recursion depth
 // :output none - calculate accessibility score
-void traverse_dom_tree(xmlNode *node, int depth) {
-    printf("depth: %d\n", depth);
-    xmlNode *cur_node = NULL;
+// void traverse_dom_tree(xmlNode *node, int depth) {
+//     printf("depth: %d\n", depth);
+//     xmlNode *cur_node = NULL;
 
-    if(NULL == node)
-    {
+//     if(NULL == node)
+//     {
+//         return;
+//     }
+//     #pragma omp parallel
+//     for (cur_node = node; cur_node; cur_node = cur_node->next) {
+//         #pragma omp task 
+//         {
+//             if (cur_node->type == XML_ELEMENT_NODE) {
+//                 // printf("Node type: Text, name: %s\n", (const char*)cur_node->name);
+//                 // print_properties(cur_node);
+//                 if (check_if_alt_needed(cur_node)) {
+//                     IMAGE_COUNT++;
+//                     check_alt_text(cur_node);
+//                 }
+//             }
+//             traverse_dom_tree(cur_node->children, depth++);
+//         }
+//     }
+// }
+
+void traverse_dom_tree(xmlNode *node, int depth) {
+    // printf("depth: %d\n", depth);
+    // xmlNode *cur_node = NULL;
+
+    if (node == NULL) {
         return;
     }
-    #pragma omp parallel
-    for (cur_node = node; cur_node; cur_node = cur_node->next) {
-        #pragma omp task 
-        {
-            if (cur_node->type == XML_ELEMENT_NODE) {
-                // printf("Node type: Text, name: %s\n", (const char*)cur_node->name);
-                // print_properties(cur_node);
-                if (check_if_alt_needed(cur_node)) {
-                    IMAGE_COUNT++;
-                    check_alt_text(cur_node);
+
+    if (depth == 0) {
+        #pragma omp parallel
+        for (xmlNode *cur_node = node; cur_node != NULL; cur_node = cur_node->next) {
+            #pragma omp critical
+            {
+                if (cur_node->type == XML_ELEMENT_NODE) {
+                    if (check_if_alt_needed(cur_node)) {
+                        IMAGE_COUNT++;
+                        check_alt_text(cur_node);
+                    }
+                }
+            }
+            traverse_dom_tree(cur_node->children, depth++);
+        }
+    } else {
+        for (xmlNode *cur_node = node; cur_node; cur_node = cur_node->next) {
+            #pragma omp critical
+            {
+                if (cur_node->type == XML_ELEMENT_NODE) {
+                    if (check_if_alt_needed(cur_node)) {
+                        IMAGE_COUNT++;
+                        check_alt_text(cur_node);
+                    }
                 }
             }
             traverse_dom_tree(cur_node->children, depth++);
